ce73e067662bf5f59a59d3e6725dc962552b2bd2
Jonathan Druet <jonathandruet2@gmail.com>
2023-09-07 09:24:35 +0200

    salle dto + form + modification in salle

--- src/main/java/com/jdbk/medsync/model/form/SalleForm.java
+++ src/main/java/com/jdbk/medsync/model/form/SalleForm.java
@@ -1,10 +1,28 @@
 package com.jdbk.medsync.model.form;
 
+import com.jdbk.medsync.model.Enum.Machine;
 import com.jdbk.medsync.model.entity.Produit;
+import com.jdbk.medsync.model.entity.RendezVous;
 import com.jdbk.medsync.model.entity.Salle;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.OneToMany;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class SalleForm {
+    private Long id;
+
+    private int etage;
+    private String numeroSalle;
+    private Machine machine;
+    private Set<RendezVous> rendezVous = new HashSet<>();
     public Salle toEntity() {
-        return null;
+        Salle salle = new Salle();
+        salle.setEtage(etage);
+        salle.setNumeroSalle(numeroSalle);
+        salle.setMachine(machine);
+        salle.setRendezVous(rendezVous);
+        return salle;
     }
 }
