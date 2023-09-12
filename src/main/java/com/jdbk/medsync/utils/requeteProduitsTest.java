b10c264f53e7d9401ef5ead39e20d810d42d6572
Jonathan Druet <jonathandruet2@gmail.com>
2023-09-12 08:29:12 +0200

    liottle update pour demande et les re set dans le produit quand on update

---
+++ src/main/java/com/jdbk/medsync/utils/requeteProduitsTest.java
@@ -0,0 +1,27 @@
+package com.jdbk.medsync.utils;
+
+public class requeteProduitsTest {
+//    INSERT INTO Produit (produit_libele, produit_quantite, produit_date_expiration)
+//    VALUES
+//            ('Aspirin', 100, NOW() + INTERVAL '3 months' + (random() * 9 || ' months')::INTERVAL),
+//            ('Ibuprofen', 200, NOW() + INTERVAL '3 months' + (random() * 9 || ' months')::INTERVAL),
+//            ('Paracetamol', 150, NOW() + INTERVAL '3 months' + (random() * 9 || ' months')::INTERVAL),
+//            ('Lipitor', 50, NOW() + INTERVAL '3 months' + (random() * 9 || ' months')::INTERVAL),
+//            ('Viagra', 20, NOW() + INTERVAL '3 months' + (random() * 9 || ' months')::INTERVAL),
+//            ('Prozac', 30, NOW() + INTERVAL '3 months' + (random() * 9 || ' months')::INTERVAL),
+//            ('Zoloft', 40, NOW() + INTERVAL '3 months' + (random() * 9 || ' months')::INTERVAL),
+//            ('Tylenol', 100, NOW() + INTERVAL '3 months' + (random() * 9 || ' months')::INTERVAL),
+//            ('Advil', 150, NOW() + INTERVAL '3 months' + (random() * 9 || ' months')::INTERVAL),
+//            ('Allegra', 60, NOW() + INTERVAL '3 months' + (random() * 9 || ' months')::INTERVAL),
+//            ('Claritin', 70, NOW() + INTERVAL '3 months' + (random() * 9 || ' months')::INTERVAL),
+//            ('Zyrtec', 80, NOW() + INTERVAL '3 months' + (random() * 9 || ' months')::INTERVAL),
+//            ('Xanax', 25, NOW() + INTERVAL '3 months' + (random() * 9 || ' months')::INTERVAL),
+//            ('Ambien', 35, NOW() + INTERVAL '3 months' + (random() * 9 || ' months')::INTERVAL),
+//            ('Benadryl', 45, NOW() + INTERVAL '3 months' + (random() * 9 || ' months')::INTERVAL),
+//            ('Ritalin', 15, NOW() + INTERVAL '3 months' + (random() * 9 || ' months')::INTERVAL),
+//            ('Adderall', 10, NOW() + INTERVAL '3 months' + (random() * 9 || ' months')::INTERVAL),
+//            ('Ativan', 20, NOW() + INTERVAL '3 months' + (random() * 9 || ' months')::INTERVAL),
+//            ('Crestor', 30, NOW() + INTERVAL '3 months' + (random() * 9 || ' months')::INTERVAL),
+//            ('Celebrex', 40, NOW() + INTERVAL '3 months' + (random() * 9 || ' months')::INTERVAL);
+//
+}
